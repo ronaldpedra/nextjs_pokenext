93d1eeed818f18b86d585b6e40d37683ac4515de
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-26 01:05:08 -0300

    Initial commit

--- src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import styles from "../../css/layout/Footer.module.css"
-
-export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <p>PokeNext &copy; June 2024</p>
-        </footer>
-    )
-}
--- src/components/layout/Main.js
+++
@@ -1,7 +0,0 @@
-import styles from "../../css/layout/Main.module.css"
-
-export default function Main({ children }) {
-    return (
-        <div className={styles.main_container}>{children}</div>
-    )
-}
