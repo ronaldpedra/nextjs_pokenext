b4173c2b20612717e85b685debedd0dd1ba12631
ronaldpedra <ronaldpedra@gmail.com>
2024-06-26 15:44:57 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,34 @@
 import styles from "../styles/core/Home.module.css";
 
+const getData = async () => {
+  const maxPokemons = 251;
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const resp = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await resp.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+};
+
 export default async function Home() {
+  const resultado = await getData();
 
-  const getData = async () => {
+  console.log(resultado.props.pokemons);
 
-    const maxPokemons = 251
-    const api = 'https://pokeapi.co/api/v2/pokemon/'
-
-    const resp = await fetch(`${api}/?limit=${maxPokemons}`)
-    const data = await resp.json()
-
-    data.results.forEach((item, index) => {
-      item.id = index + 1
-    });
-    
-    return data
-  }
-
-  const resultado = await getData()
-
-  console.log(resultado)
-  
   return (
     <>
       <h1>PokeNext</h1>
+      {resultado.props.pokemons.map((pokemon) => (
+        <p>{pokemon.name} - {pokemon.url}</p>
+      ))}
     </>
   );
 }
