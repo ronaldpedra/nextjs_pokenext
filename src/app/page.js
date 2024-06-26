041341a9a6948b57303b6cfa30eaee19880f1fa6
ronaldpedra <ronaldpedra@gmail.com>
2024-06-26 15:20:45 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,18 @@ import styles from "../styles/core/Home.module.css";
 
 export default function Home() {
 
+  const getData = async () => {
+
+    const maxPokemons = 251
+    const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+    const resp = await fetch(`${api}/?limit=${maxPokemons}`)
+    const data = await resp.json()
+    
+    return data
+  }
+
+  console.log(getData())
   
   return (
     <>
