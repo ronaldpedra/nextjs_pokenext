ed3b87b4fc1d2cb289c7f4be4eb124979b563741
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-27 12:58:28 -0300

    Update

--- src/components/wraped/PokeCard.js
+++ src/components/wraped/PokeCard.js
@@ -5,12 +5,14 @@ import styles from "../../styles/wraped/PokeCard.module.css"
 export default function PokeCard({ pokemon }) {
     return (
         <div className={styles.pokecard}>
+            <div className={styles.img_container}>
             <Image
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
             width={120}
             height={120}
             alt={pokemon.name}
             />
+            </div>
             <p>#{pokemon.id}</p>
             <h3>{pokemon.name}</h3>
             <Link href={`/pokemon/${pokemon.id}`}>Detalhes</Link>
